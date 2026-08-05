089a93ba57566ff8eb7b96e5ac79bb92a7f59916
Emilia Ortiz <emi.ozent@gmail.com>
2026-08-05 19:18:53 -0400

    fix: update API URL structure to include versioning and enhance task URLs

--- api/tasks/urls.py
+++ api/tasks/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from django.urls import path, include
+from .views import TaskViewSet, TaskReportAPIView
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path("report/", TaskReportAPIView.as_view(), name="task-report"),
+]

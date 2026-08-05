0f724abde60c965e2c1e2667e3c0df83b2b46c83
Emilia Ortiz <emi.ozent@gmail.com>
2026-08-05 18:38:39 -0400

    feat: add TaskReportRequestSerializer for enhanced task reporting functionality

--- api/tasks/serializers.py
+++ api/tasks/serializers.py
@@ -5,3 +5,28 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+        
+class TaskReportRequestSerializer(serializers.Serializer):
+    format = serializers.CharField(
+        default="written",
+        required=False,
+    )
+    include_completed = serializers.BooleanField(
+        default=False,
+        required=False,
+    )
+    language = serializers.CharField(
+        default="es",
+        required=False,
+    )
+    task_ids = serializers.ListField(
+        child=serializers.IntegerField(),
+        required=False,
+        default=list,
+    )
+    prompt = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        default="",
+    )
